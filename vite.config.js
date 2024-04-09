cbf8a55d209e5c0a8207c5883e61ef02cd6fdcc5
Nayan Thapa Magar <thapamagarnayan393@gamail.com>
2024-04-09 14:54:33 +0545

    vite configuration

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,35 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   css: {
+//     modules: {
+//       localsConvention: "camelCase",
+//     },
+//   },
+// });
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs/promises';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  css: {
-    modules: {
-      localsConvention: "camelCase",
+  esbuild: {
+    loader: 'jsx',
+  },
+  resolve: {
+    alias: {
+      './runtimeConfig': './runtimeConfig.browser',
     },
   },
-});
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
+})
